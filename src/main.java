2371698f37b001a239a253e5978b3f83b672bbd9
blaszu <blazej.lab@wp.pl>
2021-03-11 23:03:14 +0100

    lab01 - wersja2

--- src/main.java
+++ src/main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Wersja JDK: 1.8.0_111, wersja JRE: 1.7");
+        System.out.println("Labolatorium z programowania 2 - cwiczenie 01");
     }
 }
