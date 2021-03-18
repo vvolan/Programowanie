6a2bef9682b149367ebab53b5a53a601ade3929f
vvolan <jakub.w222@wp.pl>
2021-03-18 22:55:43 +0100

    lab2

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main(String[] args) {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
         Date dateToEnd1 = new Date(System.currentTimeMillis() + 7200000);
-        Date dateToEnd2 = new Date(System.currentTimeMillis() + 54000000);
-        Date dateToEnd3 = new Date(System.currentTimeMillis() + 6008000);
+        Date dateToEnd2 = new Date(System.currentTimeMillis() + 54300000);
+        Date dateToEnd3 = new Date(System.currentTimeMillis() + 98308000);
 
         Task task1 = new Task("Fryzjer", "Umówić wizytę", taskCategory.NEW, dateToEnd1.toString());
         Task task2 = new Task("Zakupy", "Kupić mleko , ser , szynke", taskCategory.IN_PROGRESS, dateToEnd2.toString());
         Task task3 = new Task("Sprzątanie", "Odkurzyć mieszkanie", taskCategory.DONE, dateToEnd3.toString());
-        Date dateToEnd4 = new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(60));
+        Date dateToEnd4 = new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(90));
         Task task4 = new Task("Jazda na rowerze", "Przejechać 40km, taskCategory.NEW, dateToEnd4.toString());
 
         List<Task> taskList = new ArrayList<Task>();
---
+++ src/taskCategory.java
@@ -0,0 +1,5 @@
+public enum taskCategory {
+    NEW,
+    IN_PROGRESS,
+    DONE
+}
