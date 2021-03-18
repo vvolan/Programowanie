27cd97874fa19f6f25fce338d3d801d899087b03
vvolan <jakub.w222@wp.pl>
2021-03-18 22:29:42 +0100

    lab2

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,13 @@ public class Main {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
         Date dateToEnd1 = new Date(System.currentTimeMillis() + 7200000);
         Date dateToEnd2 = new Date(System.currentTimeMillis() + 54000000);
-        Date dateToEnd3 = new Date(System.currentTimeMillis() + 43006000);
-        Task task1 = new Task("Fryzjer", "Umówić wizytę, taskCategory.NEW, dateToEnd1.toString());
+        Date dateToEnd3 = new Date(System.currentTimeMillis() + 96008000);
+
+        Task task1 = new Task("Fryzjer", "Umówić wizytę", taskCategory.NEW, dateToEnd1.toString());
         Task task2 = new Task("Zakupy", "Kupić mleko , ser , szynke", taskCategory.IN_PROGRESS, dateToEnd2.toString());
         Task task3 = new Task("Sprzątanie", "Odkurzyć mieszkanie", taskCategory.DONE, dateToEnd3.toString());
         Date dateToEnd4 = new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(60));
-        Task task4 = new Task("Jazda na rowerze", "Przejechać 40km", taskCategory.NEW, dateToEnd4.toString());
+        Task task4 = new Task("Jazda na rowerze", "Przejechać 40km, taskCategory.NEW, dateToEnd4.toString());
 
         List<Task> taskList = new ArrayList<Task>();
         taskList.add(task1);
